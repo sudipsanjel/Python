cb7bb120f3770c024c8a8dc360ad64a8f990dcb8
sudipsanjel <sudipsanjel.np@gmail.com>
2025-11-29 13:37:07 +0545

    Indroduction

--- 1-introduction.py
+++ 1-introduction.py
@@ -1,1 +1,42 @@
+#introduction
+
 print("hello worls")
+
+
+################################################
+#python syntax
+#execute python syntax 
+print("Hello World")
+
+#pyton indentation 
+#indentation refers to the space at the beggining of the code 
+#Example
+
+if 5 > 2:
+    print("Five is grater than two")
+
+"""if 5 > 2:
+   print("Five is greater than two!")
+"""
+
+#wrong approach it gives error because of the indentation.
+
+#########################################################
+
+#Python Variablre
+#python identify the data type of variable automatically while creating the variable
+
+#example
+x = 3
+y = "hello"
+print("This is the integer:",x)
+print("This is the string:",y)
+print(type(x))
+print(type(y))
+
+##########################################################
+#comment in python 
+# is used for single line comment 
+# ''' ''' or """" """" is used for doub;e ;ine comment
+
+

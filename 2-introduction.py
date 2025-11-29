56dfadc82da6d9ae84b78eeb943fbe6cd2a32e68
sudipsanjel <sudipsanjel.np@gmail.com>
2025-11-29 14:25:58 +0545

    introduction

---
+++ 2-introduction.py
@@ -0,0 +1,17 @@
+#VARIABLES
+
+x= 5
+y = 'sudip'
+z = 3.1415
+
+print(x,y,z)
+
+
+#CASTING
+#if you want to specify the data type of variable this can be done using casting
+
+x = str(3)
+y = float(3)
+z = int(3.14)
+
+print(x,y,z)

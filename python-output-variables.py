4418718780e3c1ce28063bc3359b2e64abd49be1
sudipsanjel <sudipsanjel.np@gmail.com>
2025-11-29 16:23:00 +0545

    Introduction

---
+++ python-output-variables.py
@@ -0,0 +1,14 @@
+#Output variablres 
+#the print function is used to output variables.
+x = "I am learning pyton"
+print(x)
+
+#print function is used to display multiple variable at same time.
+y = "I am learning variables"
+z= "this is an example"
+
+print(x,y,z)
+
+
+#We can also use + sign to add a variables or concatination of strings.
+print(x + y + z)

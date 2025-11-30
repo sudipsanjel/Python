e8792700e6ae59c2f71152dae7873e493a8aef9f
sudipsanjel <sudipsanjel.np@gmail.com>
2025-11-30 12:39:51 +0545

    Python

---
+++ 6.python-casting.py
@@ -0,0 +1,16 @@
+#IN Python casting means converting one data type into another
+#float into integer 
+#integer into float
+#complex into integer and float 
+#string into integer and floats
+
+
+############################################################################################################
+#Example
+x = int(3.339)   #3.339 will be converted into integer
+y = float(1234)  #1234 will be converted into float by showing 1234.0
+z = int("44")   #Here 44 is represented as an string it convert string into integer
+a = float("44") #44 string will be converted into floating number
+b = str(2)  #2 integer type will be converted into string as '2'
+c = str(2.22)  #2.22 floating number will be converted into string as '2.22'
+print(x,y,z,a,b,c)

bf674958a55dc297c64af78f20592a58af45a2c9
sudipsanjel <sudipsanjel.np@gmail.com>
2025-11-30 13:12:11 +0545

    Python

--- 8.python-slicing-string.py
+++ 8.python-slicing-string.py
@@ -11,7 +11,7 @@ b = "Hello Worls"
 print(b[2:7])
 """
 start --->2
-end ----->7
+end ----->7  ( ending string is not included in every  case)
 use indexing to confirm yourself
 """
 
@@ -28,5 +28,8 @@ print(b[4 : ])  #it slice of skip the starting character and goes all the way to
 ############################################################################################################
 
 #Negative Indexing
-print(b[-5 : -2])
+print(b[-5 : -2]) #starting from -5 which is W and end at -2 which is 'l' l is not included
+
+############################################################################################################
+
 

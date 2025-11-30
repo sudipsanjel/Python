0e414e1340e69ce7d835760a853a75319f62b08c
sudipsanjel <sudipsanjel.np@gmail.com>
2025-11-30 11:32:13 +0545

    Output

---
+++ 1.python-output-variables.py
@@ -0,0 +1,33 @@
+#Output variablres 
+#the print function is used to output variables.
+x = "I am learning pyton"
+print(x)
+
+#print function is used to display multiple variable at same time.
+y = "I am learning variables"
+z= "this is an example"
+
+print(x,y,z)
+
+
+#We can also use + sign to add a variables or concatination of strings.
+print(x + y + z)
+
+#The space between the two variables and operator affects the output spaces
+print(x+y+z)
+
+#we can observe from this output
+
+'''
+x= 5
+y= 'john'
+print(x+y)
+this gives error because of the different data types 
+'''
+
+
+
+
+
+
+

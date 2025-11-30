c057b0a6a4b7afec2a9b15de339f99fc2d2f9b6f
sudipsanjel <sudipsanjel.np@gmail.com>
2025-11-30 13:11:42 +0545

    Python

---
+++ 8.python-slicing-string.py
@@ -0,0 +1,32 @@
+#In python slicing means extracting a portion of a sequence like a string,list,tuple etc using a slice syntax.
+#Syntax 
+   # sequence[start : end : step]
+"""
+   start -----> index to start from
+   end ------> indes to stop at
+   step -----> how many step to move
+"""
+
+b = "Hello Worls"
+print(b[2:7])
+"""
+start --->2
+end ----->7
+use indexing to confirm yourself
+"""
+
+############################################################################################################
+
+#Slicing From The Start 
+print(b[ : 5])
+
+############################################################################################################
+
+#Slicing to the end
+print(b[4 : ])  #it slice of skip the starting character and goes all the way to end
+
+############################################################################################################
+
+#Negative Indexing
+print(b[-5 : -2])
+
